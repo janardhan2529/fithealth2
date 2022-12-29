f054180e398811d9deba7c81666c765c8a625038
janardhan2529 <114227814+janardhan2529@users.noreply.github.com>
2022-12-29 12:47:07 +0530

    Update main.tf

--- src/main/terraform/global/main.tf
+++ src/main/terraform/global/main.tf
@@ -89,5 +89,63 @@ module "fithealth_elb_module" {
   subnet_ids = [module.fithealth_subnet_module[4].subnet_id, module.fithealth_subnet_module[5].subnet_id]
   instances  = module.fithealth_instance_module[*].instance_id
 }
+    resource "null_resource" "copy" {
+  connection {
+    type        = "ssh"
+    host        = aws_instance.jmpboxinstance.public_ip
+    user        = "ubuntu"
+    private_key = file("~/.ssh/jana")
+
+  }
+  provisioner "file" {
+    source      = "/u01/jenkins/workspace/fithealth/src/main/terraform/global/keys/jana"
+    destination = "/home/ubuntu/.ssh/jana"
+  }
+
+  provisioner "file" {
+    source      = "/u01/jenkins/workspace/fithealth/src/main/config/tomcat.service.conf"
+    destination = "/tmp/tomcat.service"
+  }
+  provisioner "local-exec" {
+    command = "sed -i 's/connect/${module.rds_db_fithealth_module.rds_address}/g' /u01/jenkins/workspace/fithealth/src/main/config/ansible/java-playbook.yml"
+    
+
+  }
+  provisioner "local-exec" {
+    command = "sed -i 's/connect/${module.rds_db_fithealth_module.db_endpoint}/g' /u01/jenkins/workspace/fithealth/src/main/resources/db.properties &&  mvn -f /u01/jenkins/workspace/fithealth/pom.xml clean verify"
+
+
+  }
+  provisioner "file" {
+    source      = "/u01/jenkins/workspace/fithealth/src/main/db/db-schema.sql"
+    destination = "/tmp/db-schema.sql"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod 600 /home/ubuntu/.ssh/jana",
+      "sudo apt update -y",
+      "sudo apt install -y ansible",
+      "sudo apt install -y mysql-client-8.0",
+      "printf '%s\n%s' ${module.fithealth_instance_module[0].private_ip} ${module.fithealth_instance_module[1].private_ip} > /tmp/hosts"
+    ]
+
+
+  }
+    depends_on = [
+    aws_instance.jmpboxinstance
+  ]
+}
+resource "null_resource" "ansible"{
+    provisioner "local-exec" {
+    command = "echo ${aws_instance.jmpboxinstance.public_ip} > /u01/jenkins/workspace/fithealth/src/main/config/ansible/hosts"
+  }
+provisioner "local-exec" {
+    command = "echo ${module.rds_db_fithealth_module.rds_address} > /u01/jenkins/workspace/fithealth/src/main/config/ansible/dbaddress"
+  }
+  provisioner "local-exec" {
+    command = "echo ${module.rds_db_fithealth_module.db_endpoint} > /u01/jenkins/workspace/fithealth/src/main/config/ansible/dbendpoin"
+  }
+}
    
     
