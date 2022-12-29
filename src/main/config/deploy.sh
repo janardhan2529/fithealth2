63cc59814a777fc0fbcfcb464f8bc995bc1b487e
janardhan2529 <114227814+janardhan2529@users.noreply.github.com>
2022-12-29 13:44:48 +0530

    Update deploy.sh

--- src/main/config/deploy.sh
+++ src/main/config/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 host=$(cat hosts)
 echo "$host"
-
+scp -i /u01/jenkins/workspace/fithealth/src/main/terraform/global/keys/jana /u01/jenkins/workspace/fithealth/target/fithealth2.war ubuntu@$host:/tmp/
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/jana ubuntu@$host "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook --private-key ~/.ssh/jana -i /tmp/hosts /tmp/deploy.yml"
