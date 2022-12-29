80b5b606f88b2a296c786fd4c8e4c5ada90cb336
janardhan2529 <114227814+janardhan2529@users.noreply.github.com>
2022-12-29 14:27:44 +0530

    Update sshconnection.sh

--- src/main/config/sshconnection.sh
+++ src/main/config/sshconnection.sh
@@ -2,4 +2,4 @@
 host=$(cat hosts)
 echo "$host"
 
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/jana ubuntu@$host "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook --private-key ~/.ssh/jana -i /tmp/hosts /tmp/java-playbook.yml"
+ssh -o StrictHostKeyChecking=no -i /u01/jenkins/workspace/fithealth/src/main/terraform/global/keys/jana ubuntu@$host "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook --private-key ~/.ssh/jana -i /tmp/hosts /tmp/java-playbook.yml"
